b2d7395180ee24528b026683ee6bbaa081e1b7ae
Amir Solimany <amirsoli@post.bgu.ac.il>
2024-02-10 11:39:36 +0200

    add init function

---
+++ FootballRobot.py
@@ -0,0 +1,8 @@
+class Goalkeeper:
+    def __init__(self, FIELD_LENGTH, FIELD_WIDTH, GOAL_LENGTH, GOAL_WIDTH, ROBOT_RADIUS, BALL_RADIUS):
+        self.field_length = FIELD_LENGTH
+        self.field_width = FIELD_WIDTH
+        self.goal_length = GOAL_LENGTH
+        self.goal_width = GOAL_WIDTH
+        self.robot_radius = ROBOT_RADIUS
+        self.ball_radius = BALL_RADIUS
